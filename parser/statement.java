a8c2995feda1ca8736226195425dbb86fef23277
nishanths96 <nishanths96@gmail.com>
2017-03-19 19:12:20 +0530

    a little progress in generating intermediate code

--- parser/statement.java
+++ parser/statement.java
@@ -6,28 +6,39 @@ import SymbolTable.Token;
 import SymbolTable.TokenNameConstant;
 
 public class statement extends Node{
-	public statement() throws IOException{
+	public statement(NewLabel l) throws IOException{
 		beginProcedure();
+		this.code="";
 	}
-	public void beginProcedure(){
+	public void beginProcedure() throws IOException{
 		Token token = getToken();
-		if(token.tag == TokenNameConstant.DO){
-			beginProcedure();
-			token = getToken();
-			if(token.tag == TokenNameConstant.WHILE){
-				token = getToken();
-				if(token.tag==TokenNameConstant.OPENPARAN){
-					booleanclass b = new booleanclass();
-					token = getToken();
-					if(token.tag == TokenNameConstant.CLOSEPARAN){
-						token = getToken();
-						if(token.tag == TokenNameConstant.SEMICOLON){
-							
-						}
-					}
-				}
-				
-			}
+		int t = token.tag;
+		if(TokenNameConstant.DO==t){
+			useToken(TokenNameConstant.DO);
+			NewLabel l1 = new NewLabel();
+			NewLabel l2 = new NewLabel();
+			NewLabel temps,tempe;
+			temps = start;
+			tempe = end;
+			start = l1;
+			end = l2;
+			statement s1 = new statement(l1);
+			useToken(TokenNameConstant.WHILE);
+			useToken(TokenNameConstant.OPENPARAN);
+			// booleanclass b = new booleanclass();
+			useToken(TokenNameConstant.CLOSEPARAN);
+			
+			this.code+= l1.toString()+s1.code;
+			/*this.code += booleanclass.code;		//synthetic attr
+			  if(booleanclass.true)		// inherited attr
+			  	this.code += "goto"+ l1;
+			  else
+			   	this.code = "goto"+l2;
+			*/	
+			this.code+=l2+":\n";
+			start = temps;
+			end = tempe;
 		}
+		
 	}
 }
