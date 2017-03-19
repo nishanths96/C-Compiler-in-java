ab990ede9433e0f5d9459e8789fd5fb8615030ff
nishanths96 <nishanths96@gmail.com>
2017-03-19 19:11:27 +0530

    added two new methods and an attribute
    a new method 'usetoken' to consume the current token
    a new method 'displayerror' to display the error
    a new attribute to keep a track of the line numbers

--- parser/Node.java
+++ parser/Node.java
@@ -1,30 +1,50 @@
 package parser;
 import java.io.IOException;
-
 import SymbolTable.Token;
+import SymbolTable.TokenNameConstant;
 import lexer.LexicalAnalysis;
 
 public class Node {
-	//public static int lexline;
 	public String code;
 	public static Token token;
+	protected static NewLabel start = null;
+	protected static NewLabel end = null;
 	
 	public Node(){
-		//lexline = LexicalAnalysis.lexline;
 		code = "";
 		token = null;
 	}
-	/*public void error(){
-		System.out.println("Error at around line number: "+lexline);	//use token tag and tokenameconstant
-	}*/
-	
 	public static Token getToken() throws IOException{
-		//if(token==null)
-		token = LexicalAnalysis.getNextToken();
-		return token; 
+		if(token==null)					
+			token = LexicalAnalysis.getNextToken();
+		return token; 	// return top of stack
 	}
-	public static Token setToken(){
+	public static void useToken(int tokenNum) throws IOException{
+		boolean f = true;
+		int checkCount = 0;
+		boolean checkFlag = true;
+		if(token.tag!=tokenNum){
+			while(f){
+				if(token.tag==TokenNameConstant.EOF){
+					displayError("Error: EOF reached");
+					break;
+				}
+				if(token.tag==tokenNum){
+					f = false;
+					break;
+				}
+				token = null;
+				token = LexicalAnalysis.getNextToken();
+				checkCount+=1;
+				if(checkCount>1 && checkFlag){
+					checkFlag = false;
+					displayError("Error in Line number: "+LexicalAnalysis.lexline);
+				}				
+			}
+		}
 		token = null;
-		return token;
+	}
+	private static void displayError(String string) {
+		System.out.println(string);
 	}
 }
